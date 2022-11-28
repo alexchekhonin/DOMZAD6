4cb09e71bac373da033562ed99454848a0cec9f0
Александр Чехонин <2007cheh@mail.ru>
2022-11-28 18:19:46 +0300

    z36

--- zadacha36/Program.cs
+++ zadacha36/Program.cs
@@ -4,16 +4,31 @@
 Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
+int sum1 = 0, sum2 = 0;
+
+
 FillArrayRandomNumbers(numbers);
 Console.WriteLine("Вот массив: ");
 PrintArray(numbers);
 int count = 0;
 
+for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                  sum1 = sum1 + numbers[i];
+                else
+                  sum2 = sum2 + numbers[i];
+            }
+            Console.WriteLine("Сумма элементов на нечетных позициях : {0} ", sum2);
+
+
+
+
 for (int z = 0; z < numbers.Length; z++)
-if (z % 2 != 0)
+if (numbers[z] % 2 == 0)
 count++;
 
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них нечётные");
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
 
 void FillArrayRandomNumbers(int[] numbers)
 {
