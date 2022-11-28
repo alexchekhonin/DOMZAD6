2e7b8ed0d71077b8d4a136485d173403026ec873
Александр Чехонин <2007cheh@mail.ru>
2022-11-28 18:32:10 +0300

    zadacha36

--- zadacha36/Program.cs
+++ zadacha36/Program.cs
@@ -6,41 +6,30 @@ int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 int sum1 = 0, sum2 = 0;
 
-
 FillArrayRandomNumbers(numbers);
 Console.WriteLine("Вот массив: ");
 PrintArray(numbers);
-int count = 0;
-
+//int count = 0;
 for (int i = 0; i < numbers.Length; i++)
-            {
-                if (i % 2 == 0)
-                  sum1 = sum1 + numbers[i];
-                else
-                  sum2 = sum2 + numbers[i];
-            }
-            Console.WriteLine("Сумма элементов на нечетных позициях : {0} ", sum2);
-
-
-
-
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
-
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+{
+    if (i % 2 == 0)
+        sum1 = sum1 + numbers[i];
+    else
+        sum2 = sum2 + numbers[i];
+}
+Console.WriteLine("Сумма элементов на нечетных позициях: ={0} ", sum2);
 
 void FillArrayRandomNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(100,1000);
+        numbers[i] = new Random().Next(-100, 100);
     }
 }
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
         Console.Write(numbers[i] + " ");
     }
